2b0f05e84b750fd0c93e611e0c369bfbdbadfecd
Aleh Dzenisiuk <aleh@mediamonks.com>
2022-01-05 17:35:39 +0100

    Tack.Box: allow activateOnce() calls to be skipped the first time the box is open
    This allows to freely intermix them with regular activate() calls when the
    latter are guarded by conditions. I.e. this kind of code should not not be a problem anymore:
    
    let box = tackBox.open()
    ...
    if !label.isHidden {
    	box.activateOnce(...)
    }

--- Sources/MMMTackKit/Tack.Box.swift
+++ Sources/MMMTackKit/Tack.Box.swift
@@ -10,19 +10,19 @@ extension Tack {
 	/// Simplifies management of permanent vs dynamic constraints in `updateContraints()`.
 	///
 	/// This is another take on the `Box` concept following the older `updateContraints()` pattern we used.
-	/// We are evaluating both of them as one or another might feel more natural depending on the use case.
+	/// (We are evaluating both of them here as one or another might feel more natural depending on the use case.)
 	///
 	/// # Usage:
 	///
-	/// - Add a variable in your view:
+	/// - Add a variable into your view:
 	///
 	/// ```
-	/// private let tackBox = Tack.Box()`
+	/// private let tackBox = Tack.Box()
 	/// ```
 	///
-	/// - Get access to the box first ("open" it) in your `updateConstraints()`.
-	/// This ensures that previous dynamic constraints will be deactivated (something that's forgotten sometimes)
-	/// and the new ones will be activated at once just before leaving `updateConstraints()`.
+	/// - In your `updateConstraints()` get access to the box first ("open" it).
+	/// This ensures that previous dynamic constraints are deactivated (something that's often forgotten)
+	/// and prepares the box to track the new ones:
 	///
 	/// ```
 	/// func updateConstraints() {
@@ -40,8 +40,12 @@ extension Tack {
 	/// ))
 	/// ```
 	///
-	/// Note that the code creating them will be executed only the first time `updateConstraints()` is called.
-	/// Also note that you can have multiple calls of `activateOnce()`, they all will have effect the first time.
+	/// Note that you can have multiple calls of `activateOnce()`, they will have effect only the first time
+	/// each of them is called. (Every call is identified by the code line number, i.e. no 2 calls per line
+	/// nor 2 files sharing the same box, please.)
+	///
+	/// Also note that due to the use of auto-closures this is almost as efficient as if you were using
+	/// `if`s with custom flags.
 	///
 	/// - Add dynamic constraints that might change every time `updateConstraints()` is called:
 	///
@@ -53,19 +57,36 @@ extension Tack {
 	/// }
 	/// ```
 	///
-	/// The dynamic constraints will be activated at once when the `box` accessor goes out of scope.
+	/// Note that the calls to `activateOnce()` and `activate()` can be freely intermixed,
+	/// including the case that was not supported prior to version `0.7` of this library (where it was implicitly
+	/// required that all `activateOnce()` calls were made when the box was opened for the first time):
 	///
-	/// Note that the calls to `activateOnce()` and `activate()` calls can be freely intermixed.
+	/// ```
+	/// let box = tackBox.open()
+	/// ...
+	/// if !label.isHidden {
+	/// 	box.activateOnce(...)
+	/// }
+	/// ```
 	public final class Box {
 
 		public init() {}
 
-		private var fillingFirstTime: Bool = true
-
 		/// The dynamic ones, added via `activate` of the accessor.
 		private var constraints: [NSLayoutConstraint] = []
 
-		/// The current acessor, only for diagnostics.
+		/// Identifiers (line numbers) of already processed `activateOnce()` calls.
+		///
+		/// Note that before 0.7 we used a single flag to know if all "once constraints" were installed already.
+		/// This would not allow to use `activateOnce()` calls near regular `activate()` guarded by `if`s however,
+		/// either separating the related constraints or causing hard to notice bugs.
+		///
+		/// It should be safe to assume that a box is used from the same source code file and that no more than
+		/// one calls per line are made (at least without lecturing on code formatting).
+		/// Thus code line numbers should be able to properly identify each call.
+		private lazy var linesOfSeenActivateOnceCalls = Set<Int>()
+
+		/// The current accessor, only for diagnostics.
 		private weak var openBox: OpenBox?
 
 		/// Returns the accessor object through which the constraints should be added and which ensures they
@@ -101,23 +122,28 @@ extension Tack {
 				close()
 			}
 
-			/// Activates the given constraints only the first time the box is filled in.
-			/// (Uses an autoclosure to skip creation of your constraints when accessed later.)
+			/// Activates the given constraints only once. Uses an auto-closure for efficiency.
+			///
+			/// The auto-closure allows to skip even creation of your constraints the second time this call
+			/// is touched, i.e. you don't have to track if you've added these already nor move their addition
+			/// too far (e.g. `init`) from the dynamic constraints added via `activate()`.
 			///
 			/// Can be called multiple times and mixed with regular activate() calls.
-			///
-			/// Note that the user code could use `.once { Tack.activate(...` instead,
-			/// but having this as a pair for `activate()` could make the code look consistent.
-			public func activateOnce(_ constraints: @autoclosure () -> [NSLayoutConstraint]) {
+			public func activateOnce(
+				_ constraints: @autoclosure () -> [NSLayoutConstraint],
+				_ line: Int = #line
+			) {
 
-				guard box.fillingFirstTime else {
+				guard !box.linesOfSeenActivateOnceCalls.contains(line) else {
 					// Already set, skip calling the closure.
-					// Note that we could still call it in Debug only to check that the constraints are still the same,
-					// i.e. the user code is not misusing the API.
+					// Perhaps we could call the closure in Debug to check that the constraints are still
+					// the same as before, i.e. the user code is not misusing the API.
 					return
 				}
 
-				// Could do this later, when the box is closed, but there would not be any benefits it seems.
+				box.linesOfSeenActivateOnceCalls.insert(line)
+
+				// We could activate these later when closing the box, but there would not be any benefits it seems.
 				NSLayoutConstraint.activate(constraints())
 			}
 
@@ -125,7 +151,7 @@ extension Tack {
 			/// the next time the box is opened.
 			///
 			/// (No overload accepting OrientedChain here to keep it consistent with activateOnce() where we cannot
-			/// add an overload to keep using autoclosure.)
+			/// add an overload due to the use of auto-closures.)
 			public func activate(_ constraints: [NSLayoutConstraint]) {
 				// Again, we could activate them all at once when the box is closed,
 				// but it seems like this has no benefits and might cause confusion when debugging.
@@ -141,9 +167,6 @@ extension Tack {
 				guard !closed else { return }
 
 				closed = true
-
-				// Sealing it for permanent constraints.
-				box.fillingFirstTime = false
 			}
 		}
 	}
